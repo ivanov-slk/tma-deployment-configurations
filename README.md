5a417f578d4ca8f3024423602fd02924792b185e
Slav Ivanov <ivanov.slk@abv.bg>
2023-09-24 19:27:32 +0300

    Add the planned monitoring service to the application diagram.

--- README.md
+++ README.md
@@ -6,12 +6,32 @@ The Temperature Monitoring Application is an educational Kafka app, used as a pl
 
 ## Application diagram
 
+- Text in red shows that the component is not yet implemented.
+
 ![Temperature Monitoring Application](misc/temperature_monitoring_application.png)
 
 ## Services
 
-### Data generator
+### Data Generator
+
+#### Overview
 
 Since this is mainly a playground, the downstream services are fed by a data generating service. It produces random values for temperature, humidity and atmospheric pressure, which are then processed by other services.
 
 The repository can be found [here](https://github.com/ivanov-slk/tma-data-generator).
+
+#### Current status
+
+Produces a "hello world" message.
+
+### Monitoring Dashboard
+
+#### Overview
+
+The dashboard presents some simple metrics about the data.
+
+The repository can be found [here](https://github.com/ivanov-slk/tma-data-generator).
+
+#### Current status
+
+In progress.
--- misc/app_diagram.py
+++ misc/app_diagram.py
@@ -1,17 +1,33 @@
-from diagrams import Cluster, Diagram
+from diagrams import Cluster, Diagram, Edge
 from diagrams.onprem.queue import Nats
 from diagrams.k8s.compute import Pod
-from diagrams.k8s.compute import Deployment
+from diagrams.k8s.compute import Deploy
 
-with Diagram("Temperature monitoring application", show=False):
-    nats_generated_data = Nats("generated-data")
-
-    with Cluster("Source of Data"):
-        [
+graph_attr = {
+    "bgcolor": "transparent",
+    "dpi": "64",
+}
+with Diagram(
+    "Temperature monitoring application",
+    show=False,
+    curvestyle="curved",
+    graph_attr=graph_attr,
+    filename="misc/temperature_monitoring_application",
+):
+    with Cluster("Data Generation"):
+        nats_generated_data = Nats("generated-data")
+        data_generator = [
             Pod("Data Generator 1"),
             Pod("Data Generator 2"),
             Pod("Data Generator N"),
         ] >> nats_generated_data
 
-    data_aggregator = Deployment("Data Aggregator")
-    nats_generated_data >> data_aggregator
+    dashboard_fontcolor = "firebrick"
+    with Cluster("Monitoring Dashboard", graph_attr={"fontcolor": dashboard_fontcolor}):
+        dashboard_server = [Deploy("Dashboard Server", fontcolor=dashboard_fontcolor)]
+        # nats_processed_data >> dashboard_server
+        (
+            nats_generated_data
+            >> Edge(color=dashboard_fontcolor, style="dashed")
+            >> dashboard_server
+        )
